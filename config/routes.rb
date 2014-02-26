c5eef5f350276594b2ce778661bc010b51330086
Renuka Agarwal <renuka.agarwal83@gmail.com>
2014-02-25 23:37:57 -0500

    Updating app 2.25.14

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 FinalProjectApp::Application.routes.draw do
+  devise_for :admins
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # get "welcome/index"
   root "career#index"
